f3ee694c5958069b75f56dc9db18ac2dcf9d0c53
ToroHack <torohack.github@gmail.com>
2024-02-04 21:50:13 +0100

    pep8 format

--- Dynamic Risk Assessment System/scoring.py
+++ Dynamic Risk Assessment System/scoring.py
@@ -1,26 +1,47 @@
-import pandas as pd
+"""
+This module is responsible for scoring a trained machine learning model.
+It loads a trained model from disk, uses it to make predictions on test data,
+calculates the F1 score, and saves the score to a file.
+
+Modules:
+    - pandas: Provides data manipulation capabilities.
+    - pickle: Allows for serialization of Python objects.
+    - os: Provides operating system-related functionalities.
+    - sys: Provides access to system-specific parameters and functions.
+    - json: Provides JSON file handling.
+    - logging: Enables logging for debugging and monitoring.
+    - sklearn.metrics: Scikit-learn library for machine learning metrics.
+    - training: A custom module for splitting data.
+
+Functions:
+    - load_model(model_path: str): Loads a trained model from disk.
+    - score_model(): Scores the loaded model on test data and saves the F1 score.
+
+Author: Ricard Santiago Raigada García
+Date: January, 2024
+"""
 import pickle
 import os
 import sys
 import json
 import logging
 from sklearn.metrics import f1_score
+import pandas as pd
 from training import split_data
-
-
 
 # Load config.json and get path variables
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
-with open('config.json','r') as f:
-    config = json.load(f) 
+with open('config.json', 'r') as f:
+    config = json.load(f)
 
-dataset_csv_path = os.path.join(config['output_folder_path']) 
-test_data_path = os.path.join(config['test_data_path']) 
+dataset_csv_path = os.path.join(config['output_folder_path'])
+test_data_path = os.path.join(config['test_data_path'])
 model_path = os.path.join(config['output_model_path'])
 testdata = pd.read_csv(os.path.join(test_data_path, 'testdata.csv'))
 
-def load_model(model_path: str):
+
+def load_model(model_path: str) -> object:
     """Loads the trained model from disk.
 
     Args:
@@ -46,19 +67,26 @@ def load_model(model_path: str):
         logging.error(f"Error occurred while deserializing the model: {e}")
         raise
     except Exception as e:
-        logging.error(f"An unexpected error occurred while loading the model: {e}")
+        logging.error(
+            f"An unexpected error occurred while loading the model: {e}")
         raise
 
 
-def score_model():
-    
+def score_model() -> float:
+    """
+    Load a trained machine learning model, make predictions on test data,
+    calculate the F1 score, and save the score to a file.
+
+    Returns:
+        float: The F1 score of the model on the test data.
+    """
     # Load model
     modelpath = os.path.join(model_path, 'trainedmodel.pkl')
     model = load_model(modelpath)
-    
+
     # Split data
     X_test, _, y_test, _ = split_data(testdata)
-    
+
     # Predict using the loaded model
     y_pred = model.predict(X_test)
 
@@ -74,6 +102,7 @@ def score_model():
     logging.info(f"F1 score saved to {score_file_path}")
 
     return f1
+
 
 if __name__ == '__main__':
     score_model()
