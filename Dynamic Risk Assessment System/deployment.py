59a1b35451b769add9fc3c8ab768ed3e5ba1da13
ToroHack <torohack.github@gmail.com>
2024-02-04 22:07:59 +0100

    deployment script creation

--- Dynamic Risk Assessment System/deployment.py
+++ Dynamic Risk Assessment System/deployment.py
@@ -1,27 +1,81 @@
-from flask import Flask, session, jsonify, request
-import pandas as pd
-import numpy as np
-import pickle
+"""
+Model Deployment Script
+
+This script is responsible for deploying the trained model and related files to
+the production deployment directory.
+It copies the trained model (trainedmodel.pkl), model score (latestscore.txt),
+and ingested data record (ingestedfiles.txt)
+to the production deployment directory specified in the 'config.json' file.
+
+Modules:
+    - os: Provides operating system-related functionalities.
+    - shutil: Provides file operations for copying files.
+    - json: Provides JSON file handling.
+
+Functions:
+    - deploy_model(): Copies the necessary files to the production deployment directory.
+
+Author: Ricard Santiago Raigada García
+Date: January, 2024
+"""
 import os
-from sklearn import metrics
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
+import shutil
 import json
+import logging
+import sys
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+
+with open('config.json', 'r') as f:
+    config = json.load(f)
+
+dataset_csv_path = os.path.join(config['output_folder_path'])
+prod_deployment_path = os.path.join(config['prod_deployment_path'])
+model_path = os.path.join(config['output_model_path'])
+output_folder_path = config['output_folder_path']
 
 
+def store_model_into_pickle() -> None:
+    """
+    Copy the trained model, model score, and ingested data record to the production
+    deployment directory.
 
-##################Load config.json and correct path variable
-with open('config.json','r') as f:
-    config = json.load(f) 
+    Returns:
+        None
+    """
+    # Ensure the production deployment directory exists
+    if not os.path.exists(prod_deployment_path):
+        os.makedirs(prod_deployment_path)
 
-dataset_csv_path = os.path.join(config['output_folder_path']) 
-prod_deployment_path = os.path.join(config['prod_deployment_path']) 
+    try:
+        # Copy the trained model (trainedmodel.pkl) to the production deployment
+        # directory
+        shutil.copy(
+            os.path.join(
+                model_path,
+                'trainedmodel.pkl'),
+            prod_deployment_path)
+
+        # Copy the model score (latestscore.txt) to the production deployment
+        # directory
+        shutil.copy(
+            os.path.join(
+                model_path,
+                'latestscore.txt'),
+            prod_deployment_path)
+
+        # Copy the ingested data record (ingestedfiles.txt) to the production
+        # deployment directory
+        shutil.copy(
+            os.path.join(
+                output_folder_path,
+                'ingestedfiles.txt'),
+            prod_deployment_path)
+
+        logging.info("Copied successfully")
+    except Exception as e:
+        logging.error(f"Error during deployment: {str(e)}")
 
 
-####################function for deployment
-def store_model_into_pickle(model):
-    #copy the latest pickle file, the latestscore.txt value, and the ingestfiles.txt file into the deployment directory
-        
-        
-        
-
+if __name__ == '__main__':
+    store_model_into_pickle()
